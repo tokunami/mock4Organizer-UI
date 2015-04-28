2223ceff10aac9f71d4869d059eabc7f227b61fa
Nami Tokunaga <nami.tokunaga@fujixerox.co.jp>
2015-04-28 13:05:45 +0900

    add pagecount & thumbnail path

--- public/javascripts/mapview.js
+++ public/javascripts/mapview.js
@@ -13,56 +13,70 @@ function map(data){
 	}
 
 	var authorcount = Object.keys(data.authors).length;
+	var indextag = ['groupName', 'pageNum', 'representative', 'author'];
+	var indextitle = ['文書グループ名', '関連ページ数', '代表ページ', '作成者'];
 
 	$('#groupcount').text(groupcount + '件の文書グループが見つかりました');
-	var indextitle = ['', '文書グループ名', '関連ページ数', '代表ページ', '作成者'];
 
-	$(document).ready(function() {
+	$(document).ready(function (){
 		$('#resulttable').dataTable({
 			"bFilter": false
 		});
 	});
 
+	Object.keys(indextag).forEach(function (tag_index){
+		document.getElementById(indextag[tag_index]).innerHTML = indextitle[tag_index];
+		//$('#groupName').html('グループ名');
+	});
 
-//	document.getElementById('groupName').innerHTML = 'グループ名';
-	$('#groupName').html('グループ名');
-	$('#pageNum').html('関連ページ数');
-	$('#representative').html('代表ページ');
-	$('#author').html('作成者');
-	$('#author').attr('colspan', authorcount);
-//	document.getElementById('author').colspan = authorcount;
+	document.getElementById('author').colspan = authorcount;
+//	$('#author').attr('colspan', authorcount);
 
-	function addth (value, parentId) {
+	function addth (value, parentId){
 		var authorlabel = document.createElement('th');
 //		var authorlabel = $('<th>');
 		authorlabel.innerHTML = value;
 		document.getElementById(parentId).appendChild(authorlabel);
 	}
+	
+	Object.keys(data.authors).forEach(function (author_index){
+		var sum_page_per_author = 0;
 
-	
-	
-	Object.keys(data.authors).forEach(function (index){
-		var sum = 0;
-		for (i = 0; i < groupcount; i++) {
-			sum = sum + Object.keys(data.groups[i].documents[index]).length;
+		for (var i = 0; i < groupcount; i++) {
+			sum_page_per_author = sum_page_per_author + Object.keys(data.groups[i].documents[author_index]).length;
 		}
-		addth(data.authors[index] + '<br />' + sum + '件', 'authorname');
-	});
-	
-	Object.keys(data.groups).forEach(function (index){
-		var contents = [(parseInt(index) + 1), data.groups[index].groupName, '', data.groups[index].represent.fileName];
-
-		for (var i =0; i < authorcount; i++) {
-			contents.push(Object.keys(data.groups[index].documents[i]).length);
-		}
-
-		var newtr = document.getElementById("tbody").insertRow(index);
-		newtr.id = 'group' + (parseInt(index) + 1);
-
-		for (var i = 0; i < 4 + authorcount; i++) {
-				document.getElementById(newtr.id).insertCell(i).innerHTML = contents[i];
-			}
-
+		addth(data.authors[author_index] + '<br />' + sum_page_per_author + '件', 'authorname');
 	});
 
+	var thumbnailurl = 'pictures';
+
+	Object.keys(data.groups).forEach(function (group_index){
+		var groupNumber = parseInt(group_index) + 1;
+		var represent = data.groups[group_index].represent;
+		
+		var contents = [
+			groupNumber, 
+			data.groups[group_index].groupName,
+			'',
+			'<a href="" id=""><img src="' + thumbnailurl + represent.path + 'thumb/' + represent.pageNum + '.png"></a>',
+			represent.fileName + '<br />Page:' + represent.pageNum + '<br />' + represent.timeStamp + '<br />' + data.authors[represent.author]
+		];
+		var sum_page_per_group = 0;	//関連ページ数
+
+		for (var i = 0; i < authorcount; i++) {
+			var num_of_page = Object.keys(data.groups[group_index].documents[j]).length;
+			contents.push(num_of_page);
+			sum_page_per_group = sum_page_per_group + num_of_page;
+		}
+
+		contents[2] = sum_page_per_group + '<br /> グループの中身を見る';
+
+		var newtr = document.getElementById("tbody").insertRow(group_index);
+		newtr.id = 'group' + groupNumber;
+
+		for (var i = 0; i < 5 + authorcount; i++) {
+			var newcell = document.getElementById(newtr.id).insertCell(i);
+			newcell.innerHTML = contents[i];	
+		}
+	});
 }
