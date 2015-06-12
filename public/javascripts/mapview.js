5796b6598b44486c1ee96bcc2fb670e1ef1288ad
Nami Tokunaga <nami.tokunaga@fujixerox.co.jp>
2015-06-12 15:24:03 +0900

    add searchinfo and

--- public/javascripts/mapview.js
+++ public/javascripts/mapview.js
@@ -1,27 +1,25 @@
-function map(data){
-	if (!data.groups) {
-		$('#groupcount').text('エラーです');
+var docList = {};
+var data = {};
+
+var map = function (originaldata){
+//function map(data){
+	if (!originaldata || !originaldata.groups || !originaldata.authors) {
+		$('#groupcount').text('サーバーエラーです');
+		data = null;
 		//仮おき。受け取ったdataオブジェクトが規定型でない場合の処理
 		return;
 	}
 
-	var groupcount = Object.keys(data.groups).length;
+	data = originaldata;
+
+	var groupcount = data.groups.length;
 	if (groupcount === 0) {
 		//document.querySelector("#groupcount").textContent = ('検索結果はありませんでした');
 		$('#groupcount').text('検索結果はありませんでした');
 		return;
 	}
 
-	function addth (value, parentId, row, col){
-		var label = document.createElement('th');
-//		var label = $('<th>');
-		label.innerHTML = value;
-		if (row) label.rowspan = row;
-		if (col) label.colspan = col;
-		document.getElementById(parentId).appendChild(label);
-	}
-
-	var authorcount = Object.keys(data.authors).length;
+	var authorcount = data.authors.length;
 	var indextag = ['groupName', 'pageNum', 'representative', 'author'];
 	var indextitle = ['文書グループ名', '関連ページ数', '代表ページ', '作成者'];
 
@@ -37,16 +35,14 @@ function map(data){
 	Object.keys(indextitle).forEach(function (tag_index){
 		document.getElementById(indextag[tag_index]).innerHTML = indextitle[tag_index]
 	});
-
 	//authorをauthorcountの数に分割する
 	document.getElementById('author').colspan = authorcount;
-
 	//受け取ったdataオブジェクトの各authors要素に対してページ数の合計xを算出し、'名前<br />x件'を流し込む
-	Object.keys(data.authors).forEach(function (author_index){
+	Object.keys(data.authors).forEach(function (author_index){	//authorsの数だけ繰り返し
 		var sum_page_per_author = 0;
-
-		for (var i = 0; i < groupcount; i++) {
-			sum_page_per_author = sum_page_per_author + Object.keys(data.groups[i].documents[author_index]).length;
+		for (var i = 0; i < groupcount; i++) {	//文書グループの数だけ繰り返し
+			var pagecount = data.groups[i].documents[author_index].length;	//その文書グループの中の、author_index番目のdocuments要素の数
+			sum_page_per_author = sum_page_per_author + pagecount;
 		}
 		addth(data.authors[author_index] + '<br />' + sum_page_per_author + '件', 'authorname');
 	});
@@ -70,7 +66,7 @@ function map(data){
 		var sum_page_per_group = 0;	//関連ページ数
 
 		for (var i = 0; i < authorcount; i++) {
-			var num_of_page = Object.keys(group.documents[i]).length;
+			var num_of_page = group.documents[i].length;
 			contents.push(num_of_page);
 			sum_page_per_group = sum_page_per_group + num_of_page;
 		}
@@ -85,4 +81,136 @@ function map(data){
 			newcell.innerHTML = contents[i];	
 		}
 	});
+}
+
+function addth(value, parentId, row, col){
+	var label = document.createElement('th');
+//	var label = $('<th>');
+	label.innerHTML = value;
+	if (row) label.rowspan = row;
+	if (col) label.colspan = col;
+	document.getElementById(parentId).appendChild(label);
+}
+
+
+function searchinfo(r, c){
+	if (!data || !data.groups[r] || !data.groups[r].documents[c]) {
+		console.log('wrong argument');
+		return;
+	}
+
+	if (data.groups[r].documents[c].length === 0) {
+		console.log('naide');
+		return;
+	}
+
+	docList = initdocList();
+	var groupName = data.groups[r].groupName;
+	var representdid = data.groups[r].represent.pageId;
+
+	var docinfo = Object.keys(data.groups[r].documents[c]).map(function (idx){
+		var array = [];
+		if (data.groups[r].documents[c][idx] === representdid) {
+			array[0] = 'true';
+		} else {
+			array[0] = 'false';
+		}
+		array[1] = groupName;
+		array[2] = data.groups[r].documents[c][idx];
+		return array;
+	});
+
+	docList.query = document.getElementById("qwordbox").value;
+	docList.list = docinfo;
+	return docList;
+}
+
+function searchinfobygroup(r) {
+	if (!data || !data.groups[r]) {
+		console.log('wrong argument');
+		return;
+	}
+
+	docList = initdocList();
+
+	var groupName = data.groups[r].groupName;
+	var representdid = data.groups[r].represent.pageId;
+
+	Object.keys(data.authors).forEach(function (c){
+		if (!data.groups[r].documents[c]) {
+			console.log('wrong argument');
+			return;
+		}
+
+		if (data.groups[r].documents[c].length !== 0) {
+			var docinfo = Object.keys(data.groups[r].documents[c]).map(function (idx){
+				var array = [];
+
+				if (data.groups[r].documents[c][idx] === representdid) {
+					array[0] = 'true';
+				} else {
+					array[0] = 'false';
+				}
+
+				array[1] = groupName;
+				array[2] = data.groups[r].documents[c][idx];
+				return array;
+			});
+			docList.list = docList.list.concat(docinfo);
+		}
+	});
+	docList.query = document.getElementById("qwordbox").value;
+	return docList;
+}
+
+function searchinfobyauthor(c) {
+	if (!data) {
+		console.log('wrong argument');
+		return;
+	}
+
+	docList = initdocList();
+
+	Object.keys(data.groups).forEach(function (r){
+		if (!data.groups[r] || !data.groups[r].documents[c]) {
+			console.log('wrong argument');
+			return;
+		}
+		if (!data.groups[r].documents[c]) {
+			console.log('naide');
+			return;
+		}
+		var groupName = data.groups[r].groupName;
+		var representdid = data.groups[r].represent.pageId;
+
+		if (data.groups[r].documents[c].length !== 0) {
+			var docinfo = Object.keys(data.groups[r].documents[c]).map(function (idx){
+				var array = [];
+
+				if (data.groups[r].documents[c][idx] === representdid) {
+					array[0] = 'true';
+				} else {
+					array[0] = 'false';
+				}
+
+				array[1] = groupName;
+				array[2] = data.groups[r].documents[c][idx];
+				return array;
+			});
+			docList.list = docList.list.concat(docinfo);
+		}
+	});
+	docList.query = document.getElementById("qwordbox").value;
+	console.log(docList);
+	return docList;
+}
+
+function initdocList(){
+	docList.query = '';
+	docList.list = [];
+	return docList;
+}
+
+function listviewtest(docList){
+    var listViewWindow = window.open("/lds2/list.html", "listview");
 }
